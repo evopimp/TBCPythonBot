a197ef51398af3d7441cd47d1044f28858cd5581
evopimp <31068621+evopimp@users.noreply.github.com>
2021-05-23 15:41:30 -0500

    Add files via upload

---
+++ DataConfig/DataConfig.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+public static class DataConfigVersion
+{
+    public static int Version = 8;
+}
+
+public class DataConfig
+{
+    public int Version = DataConfigVersion.Version;
+    public string Root { get; set; } = "../json/";
+
+    [JsonIgnore]
+    public string Class => System.IO.Path.Join(Root, "class/");
+    [JsonIgnore]
+    public string Path => System.IO.Path.Join(Root, "path/");
+    [JsonIgnore]
+    public string Dbc => System.IO.Path.Join(Root, "dbc/");
+    [JsonIgnore]
+    public string WorldToMap => System.IO.Path.Join(Root, "WorldToMap/");
+    [JsonIgnore]
+    public string PathInfo => System.IO.Path.Join(Root, "PathInfo/");
+    [JsonIgnore]
+    public string MPQ => System.IO.Path.Join(Root, "MPQ/");
+    [JsonIgnore]
+    public string Area => System.IO.Path.Join(Root, "area/");
+    [JsonIgnore]
+    public string PPather => System.IO.Path.Join(Root, "PPather/");
+    [JsonIgnore]
+    public string History => System.IO.Path.Join(Root, "History/");
+    [JsonIgnore]
+    public string Experience => System.IO.Path.Join(Root, "experience/");
+    [NonSerialized]
+    public const string DefaultFileName = "data_config.json";
+
+    public static DataConfig Load()
+    {
+        if(File.Exists(DefaultFileName))
+        {
+            var loaded = JsonConvert.DeserializeObject<DataConfig>(File.ReadAllText(DefaultFileName));
+            if (loaded.Version == DataConfigVersion.Version)
+                return loaded;
+        }
+
+        return new DataConfig().Save();
+    }
+
+    private DataConfig Save()
+    {
+        File.WriteAllText(DefaultFileName, JsonConvert.SerializeObject(this));
+
+        return this;
+    }
+}
