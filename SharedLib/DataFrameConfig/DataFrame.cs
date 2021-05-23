df36d4db626d50937b3e1d59d5f43019208a8e4d
evopimp <31068621+evopimp@users.noreply.github.com>
2021-05-23 15:38:37 -0500

    Add files via upload

---
+++ SharedLib/DataFrameConfig/DataFrame.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace SharedLib
+{
+    public class DataFrame
+    {
+        public Point point { get; private set; }
+        public int index { get; private set; }
+
+        public DataFrame(Point point, int index)
+        {
+            this.point = point;
+            this.index = index;
+        }
+    }
+}
