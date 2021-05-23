225d69a87cbc383a8081c93654a3449626617526
evopimp <31068621+evopimp@users.noreply.github.com>
2021-05-23 15:38:02 -0500

    Add files via upload

---
+++ Utilities/ReadDBC_CSV_WorldMapArea/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ReadDBC_CSV_WorldMapArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var transfrom = new Transform();
+            transfrom.CreateWorldMapAreaJson();
+
+            transfrom.Validate();
+
+            Console.ReadLine();
+        }
+    }
+}
